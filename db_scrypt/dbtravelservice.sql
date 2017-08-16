f96ce19a909e33c03338bd8e80f2d601cc03a155
TsovakPalakian <tsovakpalakian@gmail.com>
2017-08-16 23:01:57 +0300

    modified 'dbtravelservice.sql'

--- db_scrypt/dbtravelservice.sql
+++ db_scrypt/dbtravelservice.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `travelservice` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `travelservice`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: travelservice
@@ -527,4 +529,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-16 16:28:13
+-- Dump completed on 2017-08-16 22:53:30
